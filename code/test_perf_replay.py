40a7bce83b9a6cc8f33ea4990700b6b38652f618
David Speck <david@saduras.de>
2019-10-08 15:12:16 +0200

    Test performance of mixing push and sampling ops

--- code/test_perf_replay.py
+++ code/test_perf_replay.py
@@ -31,6 +31,26 @@ def test_performance():
     print(
         f'sample {num_sample} of size {sample_size} took {time() - start_sample_time:.3f}s')
 
+    # reset memory
+    memory = PrioritizedReplayMemory(capacity, method='rank')
+
+    start_push_time = time()
+    for i in range(num_push):
+        # Mock transition
+        s = np.random.rand(5)
+        s_next = np.random.rand(5)
+        r = np.random.rand(1)
+        a = np.random.randint(0, 5)
+        done = False
+        err = np.random.rand(1)
+
+        # push transition to memory
+        memory.push(err, (s, a, r, s_next, done))
+
+        n = min(i + 1, sample_size)
+        memory.sample(n)
+    print(f'mixed {num_push} took {time() - start_push_time:.3f}s')
+
 
 if __name__ == "__main__":
     test_performance()
