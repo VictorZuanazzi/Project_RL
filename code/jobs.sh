93c7bad73b6835aabf6a0d9dfa8e57d87c18f4f6
David Speck <david@saduras.de>
2019-10-10 14:24:18 +0200

    Match params for cartpole

--- code/jobs.sh
+++ code/jobs.sh
@@ -2,5 +2,16 @@
 
 for x in {1..10}
 do
-    python train.py --num_episodes 1000 --batch_size 64 --num_hidden 64 --lr 5e-4 --discount_factor 0.99 --replay PER --env LunarLander-v2 --buffer 10000 --pmethod prop --TAU 0.1
+    python train.py \
+        --num_episodes 1000 \
+        --batch_size 64 \
+        --num_hidden 64 \
+        --lr 5e-4 \
+        --discount_factor 0.8 \
+        --replay PER \
+        --env CartPole-v1 \
+        --buffer 10000 \
+        --pmethod rank \
+        --TAU 0.001 \
+        --seed_value $x
 done
